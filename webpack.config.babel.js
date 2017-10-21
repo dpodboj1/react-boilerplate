360ebb52777ea0c49b6651021897b6c559e77fc9
dpodboj1 <podbojec.damir@gmail.com>
2017-10-21 14:52:30 +0200

    Added new webpack script and updated webpack config file

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,22 @@
-//Webpack config file being transpiled by babel
+//webpack config file being transpiled by babel
 import webpack from 'webpack';
+import path from 'path';
 
 export default {
-  
-}
+  //entry .js file which runs through babel-polyfill
+  entry: ['babel-polyfill', './src/index.js'],
+  //output parameters
+  output: {
+    //name of the bundled file
+    filename: 'bundle.js',
+    //resolve path of bundled file
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    //define loaders for module
+    loaders: [
+      //load every .js and .jsx file and run them through babel, not including node_modules
+      { test: /\.jsx?$/, loader:'babel-loader', exclude: /node_modules/ },
+    ]
+  }
+};
