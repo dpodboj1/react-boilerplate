f2efcf3748e1e7ad7bf5966816c40c5f998bb837
dpodboj1 <podbojec.damir@gmail.com>
2017-10-21 14:57:12 +0200

    added htmlwebpackplugin support

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,17 @@
 //webpack config file being transpiled by babel
 import webpack from 'webpack';
 import path from 'path';
+//webpack plugin which injects bundled .js file to main index.html file
+import htmlwebpackplugin from 'html-webpack-plugin';
+
+//htmlwebpackplugin config
+const hwpconfig = new htmlwebpackplugin({
+  //define main .html file location
+  template: './src/index.html',
+  filename: 'index.html',
+  //where to inject the script
+  inject: 'body'
+});
 
 export default {
   //entry .js file which runs through babel-polyfill
@@ -18,5 +29,10 @@ export default {
       //load every .js and .jsx file and run them through babel, not including node_modules
       { test: /\.jsx?$/, loader:'babel-loader', exclude: /node_modules/ },
     ]
+  },
+  //define plugins
+  plugins: {
+    //use hwpconfig
+    [hwpconfig],
   }
 };
